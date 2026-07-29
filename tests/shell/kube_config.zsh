68a296211f86985b7aa3b9aaf3a835532c16cc2b
Dimitri RODRIGUES-OLIVEIRA <dimitri.rodriguesoliveira@boulanger.com>
2026-07-29 13:00:22 +0200

    test(core): reproduce the unresolved-variable bug as a case

--- tests/shell/kube_config.zsh
+++ tests/shell/kube_config.zsh
@@ -1,3 +1,8 @@
 kube_config_show() {
     ui_path "$ZSH_ENV_DIR/scripts/fmt.zsh"
 }
+
+kube_config_write() {
+    mkdir -p "$HOME/.kube"
+    printf 'current-context: %s\n' "$1" > "$HOME/.kube/config"
+}
